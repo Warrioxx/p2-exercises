4cbf1330b438f8ff025b53e5af902589d0741d82
Xander Brus <xander.brus@student.ucll.be>
2024-03-12 15:13:00 +0100

    -

--- 02-oo/01-classes/09-local-library/student.py
+++ 02-oo/01-classes/09-local-library/student.py
@@ -1,17 +1,24 @@
 class Book:
     def __init__(self, title, author):
-        pass
+        self.title = title
+        self.author = author
 
 
 class Library:
     def __init__(self, name):
-        pass
+        self.name = name
+        self.books = []
 
     def add_book(self, book):
-        pass
+        self.books.append(book)
 
     def remove_book(self, book):
-        pass
+        if book in self.books:
+            self.books.remove(book)
 
     def search_books(self, search_string):
-        pass
+        result = []
+        for i in self.books:
+            if search_string in i.title or search_string in i.author:
+                result.append(i)
+        return result
