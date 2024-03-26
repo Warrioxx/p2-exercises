1ab9dadcff43ddf4ff3a745b6c18c3c06fba8260
Xander Brus <xander.brus@student.ucll.be>
2024-03-26 15:25:33 +0100

    -

--- 02-oo/02-access-control/08-student-records/student.py
+++ 02-oo/02-access-control/08-student-records/student.py
@@ -1,9 +1,20 @@
 class Student:
     def __init__(self, name):
-        pass
+        self.name = name
+        self.courses = {}
 
     def calculate_letter_grade(self, score):
-        pass
+        if score < 60:
+            return "F"
+        elif score < 70:
+            return "D"
+        elif score < 80:
+            return "C"
+        elif score < 90:
+            return "B"
+        else:
+            return "A"
+
 
     def add_course(self, course_name, score):
         pass
