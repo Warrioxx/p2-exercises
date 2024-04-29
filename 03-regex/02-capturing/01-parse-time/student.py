4c559c59ddc53ece3f032dc2695bc70cfcec863f
Xander Brus <xander.brus@student.ucll.be>
2024-04-29 12:20:52 +0200

    -

---
+++ 03-regex/02-capturing/01-parse-time/student.py
@@ -0,0 +1,11 @@
+import re
+
+def parse_time(string):
+    match = re.fullmatch(r'(\d{2}):(\d{2}):(\d{2})(\.\d{3})?', string)
+
+    if match:
+        h, m, s, ms = match.groups('.0')
+        ms = ms[1:]  # drop dot
+        (int(h), int(m), int(s), int(ms))
+    else:
+        return None
--- 03-regex/02-capturing/02-parse-link/student.py
+++ 03-regex/02-capturing/02-parse-link/student.py
@@ -1,1 +1,9 @@
-# Write your code here
+import re
+
+def parse_link(string):
+    match = re.match(r'<a href"(.*)">(.*)</a>', string)
+    if match:
+        link, caption = match.groups()
+        return (link, caption)
+    else:
+        return None
